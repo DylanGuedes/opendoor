cfefa242c13c988bb2ed20f1b7445721bd479ea5
DylanGuedes <djmgguedes@gmail.com>
2017-10-13 11:47:05 -0300

    changes root route
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#index'
   get '/home', to: 'application#index'
   resources 'platforms'
   get '/set_platform/:platform_id', to: 'platforms#set_instance'
