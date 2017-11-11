1979c85f8f98a2d228f6270ae2e71d7cf6244a39
DylanGuedes <djmgguedes@gmail.com>
2017-11-11 15:30:02 -0200

    sidekiq routes mount
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
   root 'application#index'
   get '/home', to: 'application#index'
+  mount Sidekiq::Web => '/sidekiq'
   resources 'platforms'
   post '/initiatives/register', to: 'resources#register_initiative'
   get '/set_platform/:platform_id', to: 'platforms#set_instance'
