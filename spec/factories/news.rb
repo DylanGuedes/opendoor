ff0e0f7db6f7d1ca7c667fd47bda65357298a197
dennis <dennisjosesilva@gmail.com>
2017-11-28 14:58:38 -0200

    Test news model.

--- spec/factories/news.rb
+++ spec/factories/news.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :news do
-    worker_uuid "MyString"
-    worker 1
-    uuid "MyString"
+    worker_uuid "asdf1234"
+    worker News.workers[:catracalivre_gatherer_worker]
+    uuid "asdf1234"
     post_id 1
-    step 1
-    lat 1.5
-    lon 1.5
+    lat '-23'
+    lon '-25'
+    status News.steps[:not_registered]
+    association :platform, factory: :platform, strategy: :build
   end
 end
--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
+require 'webmock/rspec'
 
 RSpec.describe News, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'invalid params' do
+    it 'requires a platform' do
+      attrs = FactoryGirl.attributes_for(:news, platform_id: nil)
+      news = News.new(attrs)
+      expect(news).not_to be_valid
+    end
+  end
+
+  describe 'valid params' do
+    it 'works well' do
+      platform = FactoryGirl.create(:platform)
+      attrs = FactoryGirl.attributes_for(:air_quality, platform_id: platform_id)
+      news = News.new(attrs)
+      expect(news).to be_valid
+    end
+  end
+
+  describe 'registration in the platform' do
+    before do
+      @news = FactoryGirl.create(:news)
+    end
+
+    it 'ensure capabilities exist' do
+      ssrd = soft_stub_resource_data('asdf1234')
+      ssc = soft_stub_capabilities
+      @news.fetch_or_register
+      expect(ssrd).to have_been_requested.times(1)
+      expect(ssc).to have_been_requested.times(1)
+    end
+
+    it 'correctly register resources' do
+      ssrd = soft_stub_resource_data('asdf1234')
+      ssrr = soft_stub_resource_registration('asdf1234')
+      ssrds = soft_stub_resource_data_sending('asdf1234')
+      ssc = soft_stub_capabilities
+
+      @news.fetch_or_register
+      expect(@news.uuid).not_to eq(nil)
+
+      new_data = {
+        title: [{value: 'new title', timestamp: Time.now}]
+      }
+      expect(ssrds).to have_been_requested.times(0)
+      @news.send_data(new_data)
+      expect(ssrds).to have_been_requested.times(1)
+    end
+
+  end
+
 end
